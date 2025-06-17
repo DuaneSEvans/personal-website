da991c8a3f6eae47eccad63e809f922a4ac62019
Duane Evans <duanesevans@gmail.com>
2025-06-17 16:25:56 -0700

    initial stub

---
+++ src/app/bicycle-trips/page.tsx
@@ -0,0 +1,9 @@
+import Balancer from "react-wrap-balancer"
+
+export default function BicycleTrips() {
+  return (
+    <h1 className="text-4xl font-bold text-center">
+      <Balancer ratio={0.3}>Bicycle trips coming soon.</Balancer>
+    </h1>
+  )
+}
---
+++ src/app/projects/page.tsx
@@ -0,0 +1,9 @@
+import Balancer from "react-wrap-balancer"
+
+export default function Projects() {
+  return (
+    <h1 className="text-4xl font-bold text-center">
+      <Balancer ratio={0.3}>Projects coming soon.</Balancer>
+    </h1>
+  )
+}
