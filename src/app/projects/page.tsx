40cb01b1b35ce0d52a336927a80033c9641ee282
Duane Evans <duanesevans@gmail.com>
2025-06-18 14:35:59 -0700

    projects page better for mobile

--- src/app/projects/page.tsx
+++ src/app/projects/page.tsx
@@ -37,7 +37,7 @@ export default function Projects() {
       <h1 className="text-4xl font-bold text-center">
         <Balancer ratio={0.3}>Personal Projects.</Balancer>
       </h1>
-      <div className="flex gap-16 flex-wrap">
+      <div className="flex gap-16 flex-wrap justify-center">
         {projects.map((project) => (
           <ProjectItem {...project} key={project.name} />
         ))}
@@ -48,16 +48,16 @@ export default function Projects() {
 
 function ProjectItem({ name, description, link }: Project) {
   return (
-    <article className="flex flex-col gap-2 pb-2 min-w-[400px] max-w-[500px]">
-      <h2 className="text-2xl font-bold">{name}</h2>
+    <article className="flex-1 flex flex-col gap-2 pb-2 min-w-[300px] max-w-[500px]">
+      <h2 className="text-2xl font-bold text-center">{name}</h2>
       <Link href={link} target="_blank">
-        <div className="relative bg-[var(--accent-complimentary)]">
+        <div className="relative bg-[var(--accent-complimentary)] pl-1 pb-1">
           <Image
             src={`/projects/${name.toLowerCase()}.png`}
             alt={name}
             width={1000}
             height={1000}
-            className="border-2 border-[var(--accent-complimentary)] shadow-xl hover:translate-x-2 hover:-translate-y-2 transition-all duration-300"
+            className="shadow-xl hover:translate-x-2 hover:-translate-y-2 transition-all duration-300"
           />
         </div>
       </Link>
